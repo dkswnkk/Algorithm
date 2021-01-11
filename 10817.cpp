964e691407db0cf6c2d44800e4fca5427404faee
dkswnzz <jjoy9810@gmail.com>
2021-01-12 00:22:35 +0900

    Create 10817.cpp

---
+++ 10817.cpp
@@ -0,0 +1,19 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+//  
+//  https://www.acmicpc.net/problem/10817
+//  BOJ10817 세 수
+
+#include<iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	priority_queue<int,vector<int>,greater<int>>pq;
+	int A, B, C; cin >> A >> B >> C;
+	pq.push(A); pq.push(B); pq.push(C);
+	pq.pop();
+	cout << pq.top();
+	
+}
