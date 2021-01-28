d31173ad1bedbd4a554ed2944f50aeceead270ad
dkswnzz <jjoy9810@gmail.com>
2021-01-29 00:52:42 +0900

    Create 10718.cpp

---
+++ 10718.cpp
@@ -0,0 +1,15 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+//  
+//  https://www.acmicpc.net/problem/10718
+//  BOJ10718 We love kriii
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	cout << "강한친구 대한육군" << "\n" << "강한친구 대한육군";
+		
+}
