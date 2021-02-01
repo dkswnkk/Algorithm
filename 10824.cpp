a438481008f9868b91116442bf531eab42d94798
dkswnzz <jjoy9810@gmail.com>
2021-02-01 18:15:03 +0900

    update

---
+++ 10824.cpp
@@ -0,0 +1,20 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+//  
+//  https://www.acmicpc.net/problem/10824
+//  BOJ10824 네 수
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	string temp1, temp2;
+	string a, b, c, d; cin >> a >> b >> c >> d;
+	temp1 = a + b;
+	temp2 = c + d;
+	cout << stoll(temp1) + stoll(temp2);
+
+}
+
---
+++ 2743.cpp
@@ -0,0 +1,15 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+//  
+//  https://www.acmicpc.net/problem/2743
+//  BOJ2743 단어 길이 재기
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	string s; cin >> s;
+	cout << s.length();
+}
