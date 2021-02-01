250ba2dc7a9537812a877e3e0898de41daf7047d
dkswnzz <jjoy9810@gmail.com>
2021-02-01 17:24:58 +0900

    Create 18883.cpp

---
+++ 18883.cpp
@@ -0,0 +1,24 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+//  
+//  https://www.acmicpc.net/problem/18883
+//  BOJ18883 N M 찍기
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int check = 1;
+	int N, M; cin >> N >> M;
+	for (int i = 1; i <= N; i++) {
+		for (int k = 1; k <= M; k++) {
+			cout << check;
+			if (M!=k) cout << ' ';
+			check++;
+		}
+		cout << "\n";
+	}
+}
