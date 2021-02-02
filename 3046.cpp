085a63707edfd66f2109ef6a8848951339f06872
dkswnzz <jjoy9810@gmail.com>
2021-02-02 17:22:49 +0900

    Create 3046.cpp

---
+++ 3046.cpp
@@ -0,0 +1,18 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+//  
+//  https://www.acmicpc.net/problem/3046
+//  BOJ3046 R2
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int R1, S; cin >> R1 >> S;
+
+	cout << 2*S - R1;
+	
+}
