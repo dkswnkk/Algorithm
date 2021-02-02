c4729754d30266895230ec4f7edb865f29fe7d3e
dkswnzz <jjoy9810@gmail.com>
2021-02-02 17:36:01 +0900

    Create 11557.cpp

---
+++ 11557.cpp
@@ -0,0 +1,28 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+//  
+//  https://www.acmicpc.net/problem/11557
+//  BOJ11557 Yangjojang of The Year
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int T; cin >> T;
+	
+	while (T--) {
+		int N; cin >> N;
+		vector<pair<int, string>> v;
+		for (int i = 0; i < N; i++) {
+			string S; int L; cin >> S >> L;
+			v.push_back({ L,S });
+		}
+		sort(v.begin(), v.end(), greater<>());
+		cout << v.front().second<<"\n";
+	}
+}
