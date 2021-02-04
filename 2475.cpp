fe8464952314477a7d067240b158010dc217cfb0
dkswnzz <jjoy9810@gmail.com>
2021-02-04 16:09:33 +0900

    Create 2475.cpp

---
+++ 2475.cpp
@@ -0,0 +1,20 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+//  
+//  https://www.acmicpc.net/problem/2475
+//  BOJ2475 검증수
+#include <iostream>
+#include <algorithm>
+
+#define lli long long int
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	lli a, b, c, d, e; cin >> a >> b >> c >> d >> e;
+	lli ans = (lli)(pow(a, 2) + pow(b, 2) + pow(c, 2) + pow(d, 2) + pow(e, 2)) % 10;
+	cout << ans;
+
+
+}
