e6256689889a88388683cdf8cb61f59bbc512934
dkswnzz <jjoy9810@gmail.com>
2021-02-04 16:26:50 +0900

    Create 16212.cpp

---
+++ 16212.cpp
@@ -0,0 +1,24 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+//  
+//  https://www.acmicpc.net/problem/16212
+//  BOJ16212 정열적인 정렬
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	vector<int>v;
+	int N; cin >> N;
+	while (N--) {
+		int a; cin >> a;
+		v.push_back(a);
+	}
+	sort(v.begin(), v.end());
+	for (int i : v) {
+		cout << i << ' ';
+	}
+}
