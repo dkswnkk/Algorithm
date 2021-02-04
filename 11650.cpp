fcd1792d5267d9a98b991eff32e8db48009e8ba1
dkswnzz <jjoy9810@gmail.com>
2021-02-04 14:52:21 +0900

    Create 11650.cpp

---
+++ 11650.cpp
@@ -0,0 +1,30 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+//  
+//  https://www.acmicpc.net/problem/11650
+//  BOJ11650 좌표 정렬하기
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool cmp(pair<int,int> a, pair<int,int> b) {
+	if (a.first != b.first) return a.first < b.first;
+	else return	a.second < b.second;
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N; cin >> N;
+	vector<pair<int, int>>v;
+	while (N--) {
+		int x, y; cin >> x >> y;
+		v.push_back({ x,y });
+	}
+	sort(v.begin(), v.end(), cmp);
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i].first << ' ' << v[i].second << "\n";
+	}
+}
