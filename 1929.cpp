112d90fc7a8d632166a01f908ef4da761ae9cc66
dkswnzz <jjoy9810@gmail.com>
2021-02-05 13:45:57 +0900

    update

---
+++ 1929.cpp
@@ -0,0 +1,32 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+//  
+//  https://www.acmicpc.net/problem/1929
+//  BOJ1929 소수 구하기
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int M, N; cin >> M >> N;
+	vector<bool>v(N + 1, true);
+	v[0] = v[1] = false;
+	for(int i=2; i<=N; i++){
+		if (v[i] == true) {
+			int j = 2;
+			while (i * j <= N) {
+				v[i * j] = false;
+				j++;
+			}
+		}
+	}
+	for (int i = M; i <= N; i++) {
+		if (v[i] == true) cout << i << "\n";
+	}
+
+}
---
+++ 2581.cpp
@@ -0,0 +1,32 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+//  
+//  https://www.acmicpc.net/problem/2581
+//  BOJ2581 소수
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int M, N; cin >> M >> N;
+	int min = 1e4, ans = 0;
+	vector<int>v(N + 1, true);
+
+	for (int i = 2; i <= N; i++) {
+		if (v[i] == true) {
+			if (i >= M) ans += i;
+			if (i <= min&&i>=M) min = i;
+			int j = 2;
+			while (i * j<=N) {
+				v[i * j] = false;
+				j++;
+			}
+		}
+	}
+	if (ans == 0) cout << -1;
+	else cout << ans << "\n" << min;
+}
