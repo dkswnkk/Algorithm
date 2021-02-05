3905b78264b24de792b743b467edbbd3d4b0b0c7
dkswnzz <jjoy9810@gmail.com>
2021-02-05 14:22:04 +0900

    Create 15890.cpp

---
+++ 15890.cpp
@@ -0,0 +1,13 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+//  
+//  https://www.acmicpc.net/problem/15890
+//  BOJ15890 전국 대학생 프로그래밍 대회 동아리 연합 여름 대회 2018
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout << 20180729;
+}
