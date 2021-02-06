9d1631d593355a53d9a6f75b7bf1bba69d92a457
dkswnzz <jjoy9810@gmail.com>
2021-02-06 20:52:56 +0900

    Create 1259.cpp

---
+++ 1259.cpp
@@ -0,0 +1,27 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+//  
+//  https://www.acmicpc.net/problem/1259
+//  BOJ1259 팰린드롬수
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	while (true) {
+		string N; cin >> N;
+		if (N == "0") return 0;
+		int start = 0;
+		int end = N.length() - 1;
+		bool flag = true;
+		while (start <= end) {
+			if (N[start] != N[end]) flag = false;
+			start++;
+			end--;
+		}
+		if (flag) cout << "yes" << "\n";
+		else cout<< "no"<<"\n";
+	}
+}
