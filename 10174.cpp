d308ab599256ab1de639f643309bb6cc0ce5466c
dkswnzz <jjoy9810@gmail.com>
2021-02-06 20:46:21 +0900

    Create 10174.cpp

---
+++ 10174.cpp
@@ -0,0 +1,31 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+//  
+//  https://www.acmicpc.net/problem/10174
+//  BOJ10174 팰린드롬
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N; cin >> N;
+	cin.ignore();
+	while (N--) {
+		string s; 
+		getline(cin, s);
+		transform(s.begin(), s.end(), s.begin(), ::tolower);
+		int start = 0;
+		int end = s.length() - 1;
+		bool flag = true;
+		while (start <= end) {
+			if (s[start] != s[end]) flag = false;
+			start++;
+			end--;
+		}
+		if (flag) cout << "Yes"<<"\n";
+		else cout << "No"<<"\n";
+	}
+}
