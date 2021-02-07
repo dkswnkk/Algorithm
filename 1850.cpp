6ecff9e16bae647322befd14e6692711cb2a2c64
dkswnzz <jjoy9810@gmail.com>
2021-02-07 23:54:34 +0900

    update

---
+++ 1850.cpp
@@ -0,0 +1,29 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+//  
+//  https://www.acmicpc.net/problem/1850
+//  BOJ1850 최대공약수
+
+#include <iostream>
+#define ll unsigned long long
+using namespace std;
+
+ll gcd(ll A, ll B) {
+	if (B == 0) return A;
+	else return gcd(B, A % B);
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	ll A, B; cin >> A >> B;
+	string s;
+	ll C = gcd(A, B);
+
+	
+	for (ll i = 0; i < C; i++) {
+		s += "1";
+	}
+	
+	cout << s;
+	
+}
---
+++ 8393.cpp
@@ -0,0 +1,22 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+//  
+//  https://www.acmicpc.net/problem/8393
+//  BOJ8393 합
+
+#include <iostream>
+#define ll long long 
+using namespace std;
+ll dp[10001];
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int N; cin >> N;
+	dp[0] = 0; dp[1] = 1;
+	for (int i = 2; i <= N; i++) {
+		dp[i] = dp[i - 1] + i;
+	}
+	cout << dp[N];
+	
+}
