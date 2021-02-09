00a8b6afe136cd87fa5ebd4f61dd69c19895ceee
dkswnzz <jjoy9810@gmail.com>
2021-02-09 21:54:00 +0900

    Create 1789.cpp

---
+++ 1789.cpp
@@ -0,0 +1,28 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+//  
+//  https://www.acmicpc.net/problem/1789
+//  BOJ1789 수들의 합
+
+#include <iostream>
+#define ll long long 
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	ll N,sum=0,cnt=0,i=1; cin >> N;
+	
+	while (true) {
+		sum += i;
+		i++;
+		cnt++;
+		if (sum > N) {
+			cnt--;
+			break;
+		}
+	}
+	cout << cnt;
+
+
+}
