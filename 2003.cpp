f117b7c7d8ca4b1fd39b5d23fda0e45c75aab76d
dkswnzz <jjoy9810@gmail.com>
2021-02-09 16:27:16 +0900

    2021.02.09

---
+++ 2003.cpp
@@ -0,0 +1,34 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+//  
+//  https://www.acmicpc.net/problem/2003
+//  BOJ2003 수들의 합2
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	
+	vector<int>v;
+	int N, M; cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		int num; cin >> num;
+		v.push_back(num);
+	}
+	
+	int checkSum = 0, start = 0, end = 0,cnt=0;
+
+	for (start = 0; start < v.size(); start++) {	//start를 차례대로 증가시키며 반복
+		while (end<N && checkSum < M) {		//end를 가능한 만큼 이동 시키기
+			checkSum += v[end];
+			end++;
+		}
+		if (checkSum == M) cnt++;	//부분합이 M일때 카운트 증가
+		checkSum -= v[start];
+	}
+	cout << cnt;
+}
---
+++ 2018.cpp
@@ -0,0 +1,28 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+//  
+//  https://www.acmicpc.net/problem/2018
+//  BOJ2018 수들의 합5
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N; cin >> N;
+
+	int end = 1,sum=0, cnt = 0;
+	for (int i = 1; i <= N; i++) {
+
+		while (end < N && sum < N) {	//end를 가능한 만큼 이동시킨다.
+			sum += end;
+			end++;
+		}
+		if (sum == N) cnt++;	//부분 합이 정수N일때 카운터 증가
+		sum -= i;
+	}
+	cout << cnt+1;
+}
