15a4f1e28098fa6f1ad25adabe10f31d71a02468
dkswnzz <jjoy9810@gmail.com>
2021-02-14 22:34:23 +0900

    Create 11328.cpp

---
+++ 11328.cpp
@@ -0,0 +1,25 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+// 
+//  https://www.acmicpc.net/problem/101328
+//  BOJ11328 strfry
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N; cin >> N;
+	
+	while (N--) {
+		string a, b; cin >> a >> b;
+	
+		sort(a.begin(), a.end());
+		sort(b.begin(), b.end());
+		if (a==b) cout << "Possible" << "\n";
+		else cout << "Impossible" << "\n";
+	}
+}
