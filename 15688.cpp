b81cc3f35e9af46dfb62fd32d7567d9ba4675a7d
dkswnzz <jjoy9810@gmail.com>
2021-02-14 22:03:35 +0900

    Create 15688.cpp

---
+++ 15688.cpp
@@ -0,0 +1,24 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+// 
+//  https://www.acmicpc.net/problem/15688
+//  BOJ15688 수 정렬하기5
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N; cin >> N;
+	vector<int>v;
+	while (N--) {
+		int num; cin >> num;
+		v.push_back(num);
+	}
+	sort(v.begin(), v.end());
+	for (int i : v) {
+		cout << i << '\n';
+	}
+}
