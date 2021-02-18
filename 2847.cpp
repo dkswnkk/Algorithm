ef90da5bc1c9f37193c2c4a80bc7984b193c10d1
dkswnzz <jjoy9810@gmail.com>
2021-02-18 18:07:16 +0900

    Create 2847.cpp

---
+++ 2847.cpp
@@ -0,0 +1,32 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+// 
+//  https://www.acmicpc.net/problem/2847
+//  BOJ2847 게임을 만든 동준이
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N; cin >> N;
+	vector<int>v(N);
+
+	for (int i = 0; i < N; i++) {
+		cin>>v[i];
+	}
+	int cnt = 0;
+
+	for (int i = N-1; i > 0; i--) {
+		while (v[i]<=v[i-1]) {
+			cnt++;
+			v[i - 1]--;
+
+		}
+	}
+	cout << cnt;
+}
+
