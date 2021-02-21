85fb1d7cd8521d8eae1cf89dd18140fa22a61518
dkswnzz <jjoy9810@gmail.com>
2021-02-21 13:32:51 +0900

    Create 15652.cpp

---
+++ 15652.cpp
@@ -0,0 +1,37 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+//  백트래킹
+//  https://www.acmicpc.net/problem/15652
+//  BOJ15650 N과 M (4)
+#include <iostream>
+#include <vector>
+#define max 9
+
+using namespace std;
+
+int N, M;
+int arr[max];
+bool visited[max];
+
+void dfs(int num, int start) {
+	if (start == M) {
+		for (int i = 0; i < M; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << "\n";
+	}
+	else {
+		for (int i = num; i <= N; i++) {
+				visited[i] = true;
+				arr[start] = i;
+				dfs(i, start + 1);
+				visited[i] = false;
+		}
+	}
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	cin >> N >> M;
+	dfs(1, 0);
+}
