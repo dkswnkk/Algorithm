4a8d9d331d20dfe9d46f038ce0a5a3bf1252870c
dkswnzz <jjoy9810@gmail.com>
2021-03-28 19:47:01 +0900

    Create 2033.cpp

---
+++ BOJ/2033.cpp
@@ -0,0 +1,26 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+//  https://github.com/dkswnzz
+//  https://www.acmicpc.net/problem/2033
+//  BOJ2033 반올림
+
+#include <iostream>
+#include <string>
+#define ll long long 
+
+using namespace std;
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	ll n; cin >> n;
+	int t = 1;
+	while (n > t * 10) {
+		n += 5 * t;
+		t *= 10;
+		n -= n % t;
+	}
+	cout << n;
+	
+}
