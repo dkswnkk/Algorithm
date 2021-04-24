927d2f1e211b14ca68f7e58ceeba4d8cb8a4f89f
dkswnzz <jjoy9810@gmail.com>
2021-04-24 23:45:28 +0900

    Update 13414.cpp

--- BOJ/13414.cpp
+++ BOJ/13414.cpp
@@ -4,8 +4,9 @@
 //  BOJ13414 수강신청
 
 #include <iostream>
-#include <queue>
+#include <map>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
@@ -21,7 +22,7 @@ int main() {
 		int sno; cin >> sno;
 		if (find(ans.begin(), ans.end(), sno) != ans.end()) {	//중복되는 값이 있을경우
 			auto it = find(ans.begin(), ans.end(), sno) - ans.begin();
-			ans.erase(ans.begin() + it);	//삭제하고 
+			ans.erase(ans.begin() + it);	//삭제하고
 			ans.push_back(sno);	//맨뒤에 삽입
 		}
 		else ans.push_back(sno);	//중복되는 값이 없을 경우 그냥 삽입
@@ -31,5 +32,23 @@ int main() {
 	}
 	*/
 
+	map<string, int>m;
+	vector<pair<int, string>>v;
 
+	int k, L; cin >> k >> L;
+	for (int i = 0; i < L; i++) {
+		string number; cin >> number;
+		m[number] = i + 1;
+	}
+
+	for (auto i = m.begin(); i != m.end(); i++) {
+		string a = i->first;
+		int b = i->second;
+		v.push_back({ b,a });
+	}
+
+	sort(v.begin(), v.end());
+	for (int i = 0; i < k; i++) {	//출력
+		cout << v[i].second << "\n";
+	}
 }
