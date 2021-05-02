ec194e70b7117407f85922bfc80e49c767e89a9e
dkswnzz <jjoy9810@gmail.com>
2021-05-02 10:48:15 +0900

    Create 5525.cpp

---
+++ BOJ/5525.cpp
@@ -0,0 +1,34 @@
+﻿//  Copyright © 2021 안주형. All rights reserved.
+//  https://github.com/dkswnzz
+//  https://www.acmicpc.net/problem/5525
+//  BOJ5525 IOIOI
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int n, m; cin >> n >> m;
+	string s; cin >> s;
+
+	int cnt = 0, ans = 0;
+
+	for (int i = 0; i < s.length();) {
+		if (s[i] == 'I' && s[i + 1] == 'O' && s[i + 2] == 'I') {	//연속적으로 IO가 나올때
+			cnt++;
+			i += 2;
+		}
+		else {
+			if (cnt != n) cnt = 0;
+			i++;
+		}
+		if (cnt == n) {
+			ans++;
+			cnt--;
+		}
+	}
+	cout << ans;
+}
