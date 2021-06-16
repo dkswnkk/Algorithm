dc49de4138682de3ffb488ab93d56727949d08a9
안주형 <jjoy9810@gmail.com>
2021-06-17 01:43:57 +0900

    수 정렬하기2

---
+++ BOJ/java/2751.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+	public static void main(String[]args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N=Integer.parseInt(br.readLine());
+		ArrayList<Integer> arr = new ArrayList<>();
+		StringBuilder sb = new StringBuilder();
+		while(N-->0) {
+			arr.add(Integer.parseInt(br.readLine()));
+		}
+		Collections.sort(arr);
+		for(int i:arr) {
+			sb.append(i).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
