bb8106b43bb8dd4c4ca05354d22b0bcdbc409f71
안주형 <jjoy9810@gmail.com>
2021-07-05 00:56:25 +0900

    이진 탐색 트리
    https://www.acmicpc.net/problem/2957

---
+++ BOJ/java/2957.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.TreeMap;
+
+public class Main {
+
+	static long ans;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+		TreeMap<Integer, Integer> map = new TreeMap<>();
+
+		for (int i = 0; i < N; i++) {
+			int m = Integer.parseInt(br.readLine());
+
+			if (i == 0) {
+				map.put(m, 0);
+				ans = 0;
+			} else {
+				Integer upperKey = map.higherKey(m);
+				Integer lowerKey = map.lowerKey(m);
+
+				int depth = 0;
+				if (upperKey == null) {
+					depth = map.get(lowerKey) + 1;
+					map.put(m, depth);
+				} else if (lowerKey == null) {
+					depth = map.get(upperKey) + 1;
+					map.put(m, depth);
+				} else {
+					int upper = map.get(upperKey);
+					int lower = map.get(lowerKey);
+
+					depth = upper > lower ? upper + 1 : lower + 1;
+					map.put(m, depth);
+				}
+				ans += depth;
+			}
+			sb.append(ans).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
