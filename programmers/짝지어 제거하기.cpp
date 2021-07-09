fd41ef99a6a507c990b86aa10b03f58845cc9df5
안주형 <jjoy9810@gmail.com>
2021-07-09 11:02:55 +0900

    짝지어 제거하기
    https://programmers.co.kr/learn/courses/30/lessons/12973

---
+++ programmers/짝지어 제거하기.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int solution(string s)
+{
+    int answer = -1;
+    stack<char>st;
+    
+    for(int i=0; i<s.length(); i++){
+        if(!st.empty()){
+            if(st.top()==s[i]) st.pop();
+            else st.push(s[i]);
+        }
+        else st.push(s[i]);    
+    }    
+    
+    if(st.empty()) answer=1;
+    else answer=0;
+    
+    return answer;
+}
