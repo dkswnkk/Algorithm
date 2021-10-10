f5eb922e81e5e1bb980fd4c9315100c34521e76b
JuHyeong Ahn <dkswnkk.dev@gmail.com>
2021-10-11 02:36:23 +0900

    돌 게임3
    https://www.acmicpc.net/problem/9657

---
+++ BOJ/c++/9657.cpp
@@ -0,0 +1,44 @@
+//  Copyright © 2021 안주형. All rights reserved.
+//  https://github.com/dkswnkk
+//  https://www.acmicpc.net/problem/9657
+//  BOJ9657 돌 게임3
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int dp[1001];
+
+void game(int cnt){
+    if(dp[cnt]!=0) return;
+    if(cnt==1||cnt==3||cnt==4){
+        dp[cnt]=1;
+        return;
+    }
+    for(int i=1; i<=4; i++){
+        if(i==2) continue;
+        if(cnt-i>=0){
+            if(dp[cnt-i]==0) game(cnt-i);
+            if(dp[cnt-i]==-1){
+                dp[cnt]=1;
+                return;
+            }
+        }
+        
+    }
+    dp[cnt]=-1;
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int N; cin>>N;
+    
+    game(N);
+    
+    if(dp[N]==1) cout<<"SK";
+    else cout<<"CY";
+}
