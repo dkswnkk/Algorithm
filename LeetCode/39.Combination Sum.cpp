6c90a4197403efe8e8f83e4e5a41befee4003fab
JuHyeong Ahn <dkswnkk.dev@gmail.com>
2021-11-16 15:42:06 +0900

    Combination Sum
    https://leetcode.com/problems/combination-sum/

--- LeetCode/39.Combination Sum.cpp
+++ LeetCode/39.Combination Sum.cpp
@@ -1,33 +1,21 @@
 class Solution {
-    map<vector<int>,int>duplicate;
-    
 public:
     vector<vector<int>>ans;
-    void dfs(vector<int> &candidates,int &target,int sum,vector<int> route){
-        if(sum==target){
-            sort(route.begin(),route.end());
-            if(duplicate[route]==0){
-                duplicate[route]++;
-                ans.push_back(route);
-                return;
-            }
-        }
-        else if(sum>target) return;
-        else{
-            for(int i=0; i<candidates.size(); i++){
-                vector<int>temp(route);
-                temp.push_back(candidates[i]);
-                dfs(candidates,target,sum+candidates[i],temp);
-            }
-        }
-        return;
-    }
     
+    void dfs(int target,int index,vector<int>& candidates, vector<int>current){
+        if(target==0) ans.push_back(current);
+        
+        for(int i=index; i<candidates.size(); i++){
+            if(candidates[i]>target) break;
+            current.push_back(candidates[i]);
+            dfs(target-candidates[i],i,candidates,current);
+            current.pop_back();
+        }
+    }
+        
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        dfs(candidates,target,0,{});
-        sort(ans.begin(),ans.end());
-        
-        
+        sort(candidates.begin(),candidates.end());
+        dfs(target,0,candidates,{});
         return ans;
     }
 };
