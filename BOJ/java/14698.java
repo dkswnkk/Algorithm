d8b5884a08cb7f6cf7bbb0d2fb8cc42cc1e9ad2d
JuHyeong Ahn <dkswnkk.dev@gmail.com>
2021-12-24 21:11:59 +0900

    전생했더니 슬라임 연구자였던 건에 대하여 (Hard)
    https://www.acmicpc.net/problem/14698

---
+++ BOJ/java/14698.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		final long mod = 1000000007;
+		for (int t = 0; t < T; t++) {
+			int n = Integer.parseInt(br.readLine());
+			StringTokenizer	st = new StringTokenizer(br.readLine());
+			PriorityQueue<Long> pq = new PriorityQueue<Long>();
+			for (int i = 0; i <n; i++) {
+				pq.add(Long.parseLong(st.nextToken()));
+			}
+			long score = 1;
+			while (pq.size() >=2) {
+				long one = pq.poll();
+				long two = pq.poll();
+				score*=(((one%mod)*(two%mod))%mod);
+			pq.add(one*two);
+			score%=mod;
+			}
+			sb.append(score).append("\n");
+
+		}
+		System.out.print(sb);
+	}
+}
