64cfbd35f3b20c674a672649e96369799f241003
JuHyeong Ahn <dkswnkk.dev@gmail.com>
2022-03-04 12:43:57 +0900

    [성능개선] 피로도
    https://programmers.co.kr/learn/courses/30/lessons/87946

--- programmers/피로도.cpp
+++ programmers/피로도.cpp
@@ -6,26 +6,27 @@ using namespace std;
 
 int ans = 0;
 
-int backtracking(int index, int explore_cnt, int curr_point, vector<vector<int>>& dungeons){
+int backtracking(vector<int> visited, int explore_cnt, int curr_point, vector<vector<int>>& dungeons){
     
-    for(int i=index; i<dungeons.size(); i++){
+    for(int i=0; i<dungeons.size(); i++){
         int need_point = dungeons[i][0];
         int use_point = dungeons[i][1];
         
         if(curr_point>=need_point){
-            ans = max(ans, backtracking(i+1, explore_cnt+1, curr_point - use_point, dungeons));
+            if(!visited[i]){
+                visited[i]=1;
+                ans = max(ans, backtracking(visited, explore_cnt+1, curr_point - use_point, dungeons));
+                visited[i]=0;
+            } 
+
         }
     }
     return explore_cnt;
 }
 
 int solution(int k, vector<vector<int>> dungeons) {
-    sort(dungeons.begin(),dungeons.end());
+    vector<int>visited(50);
+    backtracking(visited,0,k,dungeons);
     
-    do{
-        backtracking(0,0,k,dungeons);
-    }while(next_permutation(dungeons.begin(),dungeons.end()));
-    
-    backtracking(0,0,k,dungeons);
     return ans;
 }
