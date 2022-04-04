09a060c562a7bfb303a668ef079027c7a5c5d71b
JuHyeong Ahn <dkswnkk.dev@gmail.com>
2022-04-04 21:48:42 +0900

    re: 정수 삼각형
    https://www.acmicpc.net/problem/1932

--- BOJ/c++/1932.cpp
+++ BOJ/c++/1932.cpp
@@ -32,3 +32,36 @@ int main(){
     
     cout<<ans;
 }
+
+
+===================2022.04.04===================
+#include <iostream>
+
+using namespace std;
+
+int arr[501][501];
+int dp[501][501];
+int ans;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int N; cin>>N;
+    for(int i=1; i<=N; i++){
+        for(int k=0; k<i; k++){
+            cin>>arr[i-1][k];
+        }
+    }
+    dp[0][0] = arr[0][0];
+    ans = dp[0][0];
+    
+    for(int i=1; i<N; i++){
+        for(int k=0; k<N; k++){
+            if(k==0) dp[i][k] = dp[i-1][k] + arr[i][k];
+            else if(k==N-1) dp[i][k] = dp[i-1][k-1] + arr[i][k];
+            else dp[i][k] = max(dp[i-1][k-1]+arr[i][k],dp[i-1][k]+arr[i][k]);
+            ans = max(ans, dp[i][k]);
+        }
+    }
+    cout<<ans;
+    
+}
