eb664b892140f818fadd0ed93a211c281412fe01
JuHyeong Ahn <dkswnkk.dev@gmail.com>
2022-04-09 19:10:02 +0900

    re: 숫자 문자열과 영단어
    https://programmers.co.kr/learn/courses/30/lessons/81301

--- programmers/숫자 문자열과 영단어.cpp
+++ programmers/숫자 문자열과 영단어.cpp
@@ -1,47 +1,22 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <regex>
 
 using namespace std;
 
-string change(string word){
-    if(word=="zero") return "0";
-    else if(word=="one") return "1";
-    else if(word=="two") return "2";
-    else if(word=="three") return "3";
-    else if(word=="four") return "4";
-    else if(word=="five") return "5";
-    else if(word=="six") return "6";
-    else if(word=="seven") return "7";
-    else if(word=="eight") return "8";
-    else if(word=="nine") return "9";
-    else return "-1";
-}
-
 int solution(string s) {
-    string ans;
     int answer = 0;
-    for(int i=0; i<s.length();){
-        if(s[i]>='a'&&s[i]<='z'){ //단어가 영단어일때
-            string temp;
-            int k=i;
-            while(s[k]>='a'&&s[k]<='z'){
-                temp+=s[k];
-                k++;
-                i=k;
-                if(change(temp)!="-1"){
-                    ans+=change(temp);
-                    i=k;
-                    break;
-                }
-            }
-        }
-        else{
-            ans+=s[i];
-            i++;
-        }
-    }
     
-    answer=stoi(ans);
+    s = regex_replace(s, regex("zero"),"0");
+    s = regex_replace(s, regex("one"),"1");
+    s = regex_replace(s, regex("two"),"2");
+    s = regex_replace(s, regex("three"),"3");
+    s = regex_replace(s, regex("four"),"4");
+    s = regex_replace(s, regex("five"),"5");
+    s = regex_replace(s, regex("six"),"6");
+    s = regex_replace(s, regex("seven"),"7");
+    s = regex_replace(s, regex("eight"),"8");
+    s = regex_replace(s, regex("nine"),"9");
+    answer = stoi(s);
     return answer;
 }
