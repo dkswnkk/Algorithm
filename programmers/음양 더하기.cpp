ded56f2beac9786b2ca00fed6d7c5f933b9c50b1
JuHyeong Ahn <dkswnkk.dev@gmail.com>
2022-04-15 00:16:10 +0900

    re: 음양 더하기
    https://programmers.co.kr/learn/courses/30/lessons/76501

--- programmers/음양 더하기.cpp
+++ programmers/음양 더하기.cpp
@@ -4,12 +4,10 @@
 using namespace std;
 
 int solution(vector<int> absolutes, vector<bool> signs) {
-    int answer = 123456789;
-    answer=0;
-    
+    int answer = 0;
     for(int i=0; i<absolutes.size(); i++){
-        if(signs[i]) answer+=absolutes[i];
-        else answer-=absolutes[i];
+        if(signs[i]) answer += absolutes[i];
+        else answer += (~absolutes[i])+1;
     }
     return answer;
 }
