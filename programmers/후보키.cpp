4aa1e0ba2469f23709f1d6c1976060aafab143cf
JuHyeong Ahn <dkswnkk.dev@gmail.com>
2022-04-17 03:13:07 +0900

    re: 후보키
    https://programmers.co.kr/learn/courses/30/lessons/42890

--- programmers/후보키.cpp
+++ programmers/후보키.cpp
@@ -5,8 +5,8 @@
 #include <algorithm>
 
 using namespace std;
-map<string,int> duplicate_candidate_table;
-vector<string> duplicate_candidate_vec;
+map<string,int> uniqueness_table;
+vector<string> uniqueness_vec;
 set<string> candidate;
 
 
@@ -15,34 +15,18 @@ bool cmp(string s1, string s2){
     return s1<s2;
 }
 
-bool check(string inp){
-    if(duplicate_candidate_table.find(inp)!=duplicate_candidate_table.end()) return false;    //이미 유일성은 만족하는게 있음
-    return true;
-}
-
 void combination(int index, vector<vector<string>> relation, vector<int> pick){
     map<string,int>temp;
-    
-    bool flag = true;
     for(int i=0; i<relation.size(); i++){
         string inp = "";
-        string num = "";
-        for(int k=0; k<pick.size(); k++){
-            inp += relation[i][pick[k]];
-            num+=to_string(pick[k]);
-            if(!check(num)){
-                flag = false;;
-                break;
-            }
-        }
+        for(int k=0; k<pick.size(); k++) inp += relation[i][pick[k]];
         temp[inp]++;
-        if(!flag)break;
     }
     
     if(temp.size() == relation.size()){
         string inp = "";
         for(int num:pick) inp += to_string(num);
-        duplicate_candidate_table[inp] = 1;
+        uniqueness_table[inp] = 1;
         return;
     }
     for(int i=index+1; i<relation[0].size(); i++){
@@ -52,27 +36,29 @@ void combination(int index, vector<vector<string>> relation, vector<int> pick){
     }
 }
 
-int solution(vector<vector<string>> relation) {
-    for(int i=0; i<relation[0].size(); i++) combination(i, relation, {i});
+void minimality_check(){
+    candidate.insert(uniqueness_vec[0]);
     
-    for(auto it = duplicate_candidate_table.begin(); it!= duplicate_candidate_table.end(); it++) duplicate_candidate_vec.push_back(it->first);
-    sort(duplicate_candidate_vec.begin(),duplicate_candidate_vec.end(),cmp);
-    
-    for(int i=0; i<duplicate_candidate_vec.size(); i++){
-        if(candidate.empty()){
-            candidate.insert(duplicate_candidate_vec[i]);
-            continue;
-        }
+    for(int i=1; i<uniqueness_vec.size(); i++){
         bool flag = true;
         for(auto it = candidate.begin(); it!=candidate.end(); it++){
             int cnt = 0;
             for(int k=0; k<it->length(); k++){
-                if(duplicate_candidate_vec[i].find(it->data()[k])!=string::npos) cnt++;
+                if(uniqueness_vec[i].find(it->data()[k])!=string::npos) cnt++;
             }
             if(cnt == it->length()) flag = false;
         }
-        if(flag) candidate.insert(duplicate_candidate_vec[i]);
+        if(flag) candidate.insert(uniqueness_vec[i]);
     }
-    return candidate.size();
 }
 
+int solution(vector<vector<string>> relation) {
+    for(int i=0; i<relation[0].size(); i++) combination(i, relation, {i});
+    
+    for(auto it = uniqueness_table.begin(); it!= uniqueness_table.end(); it++) uniqueness_vec.push_back(it->first);
+    
+    sort(uniqueness_vec.begin(),uniqueness_vec.end(),cmp);
+    minimality_check();
+    
+    return candidate.size();
+}
