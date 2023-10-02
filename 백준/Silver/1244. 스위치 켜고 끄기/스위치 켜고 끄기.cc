91667a4b10fd614bffd1318e56ea34df735967b4
JuHyeong Ahn <jjoy9810@gmail.com>
2023-10-02 23:04:06 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int arr[101];
+int N;
+
+void boy(int num){
+    for(int i=num; i<=N; i++){
+        if(i % num == 0){
+            if(arr[i] == 0) arr[i] = 1;
+            else arr[i] = 0;
+        }
+    }
+}
+
+void girl(int num){
+    int left = num - 1;
+    int right = num + 1;
+    while(true){
+        if(left < 1 || right > N) break;
+        if(arr[left] == arr[right]){
+            left--;
+            right++;
+        }
+        else break;
+    }
+    left++;
+    right--;
+    for(int i=left; i<=right; i++){
+        if(arr[i] == 0) arr[i] = 1;
+        else arr[i] = 0;
+    }
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    cin>>N;
+    for(int i=1; i<=N; i++) cin>>arr[i];
+    int peo; cin>>peo;
+    for(int i=0; i<peo; i++){
+        int a, b; cin>>a>>b;
+        if(a == 1) boy(b);
+        else if(a == 2) girl(b);
+    }
+    for(int i=1; i<=N; i++){
+        cout<<arr[i] <<' ';
+        if(i % 20 == 0) cout<<'\n';
+    }
+    
+}
+
