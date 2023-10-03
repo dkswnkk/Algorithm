456b5c34f34ddb79eab3bd87ef7a9b87871c36b6
JuHyeong Ahn <jjoy9810@gmail.com>
2023-10-03 12:25:41 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.cc
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -1,19 +1,27 @@
-//  Copyright © 2021 안주형. All rights reserved.
-//  https://github.com/dkswnkk
-//  https://www.acmicpc.net/problem/9655
-//  BOJ9655 돌 게임
-
 #include <iostream>
-
 using namespace std;
 
+
+int dp[1001];
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     
     int N; cin>>N;
-
-    cout<<(N%2?"SK":"CY");    //N=홀수, 상근 승, 짝수, 창근 승
     
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for(int i=3; i<=N; i++){
+        dp[i] = min(dp[i-1] + 1, dp[i-3] + 1);
+    }
+    
+    if(dp[N] % 2 == 0){
+        cout<<"CY";
+    }
+    else{
+        cout<<"SK";
+    }
 }
